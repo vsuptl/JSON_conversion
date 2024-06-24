04d048bf42e64abad1e6b272df5f2062447f44ea
vsuptl <vpate217@uic.edu>
2024-06-24 11:33:06 -0500

    Obtained the first section in JSON file

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import csv
 
 file_path = 'ctg-studies.json'
 
-with open(file_path, 'r') as file:
+with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
+
+first_section = data[0].get("protocolSection")
+
+print(first_section)
